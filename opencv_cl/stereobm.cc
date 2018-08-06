2038dd5fc70b7d3555d3af979d63111f301d10e1
GaoHongchen <1656738573@qq.com>
2018-08-06 11:19:58 +0800

    add opencv_cl

---
+++ opencv_cl/stereobm.cc
@@ -0,0 +1,37 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ocl::setUseOpenCL(true);
+
+    namedWindow("disp",1);
+
+    Mat left, right, disp;
+    UMat uleft, uright, udisp;
+
+    left  = cv::imread("/home/cg/Downloads/Cloth3/disp1.png", IMREAD_GRAYSCALE);
+    right = cv::imread("/home/cg/Downloads/Cloth3/disp5.png", IMREAD_GRAYSCALE);
+
+    left.copyTo(uleft);
+    right.copyTo(uright);
+
+    Ptr<StereoBM> bm = StereoBM::create(0, 21);
+    bm->setPreFilterType(bm->PREFILTER_XSOBEL);
+    bm->setTextureThreshold(0);
+
+    bm->compute(left, right, disp);
+    bm->compute(uleft, uright, udisp);
+
+    disp.convertTo(disp, CV_8UC1, 255, 0);
+
+    imshow("disp", disp);
+
+    cv::waitKey();
+
+    return 0;
+}
